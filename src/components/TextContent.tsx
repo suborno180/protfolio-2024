efa1076f931ee3c5e33c8f93bfca0d85869fbe92
suborno180 <64451255+suborno180@users.noreply.github.com>
2024-04-19 09:55:23 +0600

    update

--- src/components/TextContent.tsx
+++ src/components/TextContent.tsx
@@ -26,7 +26,7 @@ const TextContent = () => {
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1, delay: 1 }}
           >
-            The technological revolution is changing aspect of our lives, and the fabric of society itself. it's also changing the way we learn and what we learn. Factual knowledge is less prized when everything you ever need to know can be found on your phone. There's no imperative to be an expert at doing everything when you can.
+           {` The technological revolution is changing aspect of our lives, and the fabric of society itself. it's also changing the way we learn and what we learn. Factual knowledge is less prized when everything you ever need to know can be found on your phone. There's no imperative to be an expert at doing everything when you can.`}
           </motion.p>
         </div>
       </motion.div>
