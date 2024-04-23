2af0b4715b94928a295c9b0dfb7904d49ce913d9
suborno180 <64451255+suborno180@users.noreply.github.com>
2024-04-23 14:21:14 +0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,101 @@ import ExperienceSection from "@/components/sections/ExperienceSection";
 import HeroSection from "@/components/sections/HeroSection";
 import MySkiles_1 from "@/components/sections/MySkiles_1";
 import ProjectsSection from "@/components/sections/ProjectsSection";
-import Header from "@/components/Header"
+import Header from "@/components/Header";
 import CountingSection from "@/components/sections/CountingSection";
 
 export default function Home() {
+  const allExperiences = [
+    {
+      src: "/experiences/html5-logo.png",
+      alt: "HTML5"
+    },
+    {
+      src: "/experiences/css-logo.png",
+      alt: "CSS3",
+    },
+    {
+      src: "/experiences/sass-1-logo.png",
+      alt: "CSS3",
+    },
+    {
+      src: "/experiences/javascript-logo.png",
+      alt: "JavaScript",
+    },
+    {
+      src: "/experiences/Node.js_logo.png",
+      alt: "Node.js",
+    },
+    {
+      src: "/experiences/React.webp",
+      alt: "React",
+    },
+    {
+      src: "/experiences/ExpressJS-logo.png",
+      alt: "ExpressJS",
+    },
+    {
+      src: "/experiences/nextjs-icon.png",
+      alt: "nextjs",
+    },
+    {
+      src: "/experiences/puppeteer-icon.png",
+      alt: "Puppeteer",
+    },
+    {
+      src: "/experiences/redux-logo.png",
+      alt: "Redux",
+    },
+    {
+      src: "/experiences/framer-motion-logo.png",
+      alt: "framer-motion",
+    },
+    {
+      src: "/experiences/Figma-Logo.png",
+      alt: "Figma",
+    },
+    {
+      src: "/experiences/Tailwind_CSS_logo.svg.png",
+      alt: "Tailwind_CSS",
+    },
+    {
+      src: "/experiences/bootstrap-logo.png",
+      alt: "Bootstrap",
+    },
+    {
+      src: "/experiences/Mongo_DB.png",
+      alt: "Mongo_DB",
+    },
+    {
+      src: "/experiences/prisma-2.svg",
+      alt: "Prisma ORM",
+    },
+    {
+      src: "/experiences/adobe-photoshop-logo.png",
+      alt: "Adobe-Photoshop",
+    },
+    {
+      src: "/experiences/adobe-illustrator-logo.png",
+      alt: "Adobe-Illustrator",
+    },
+    {
+      src: "/experiences/Wordpress_Blue_logo.png",
+      alt: "Wordpress",
+    },
+  ]
   return (
     <>
       <main>
-        <Header/>
+        <Header />
         <HeroSection />
-        <ExperienceSection />
+        <ExperienceSection
+          title="My Experiences"
+          logos={allExperiences}
+        />
         <MySkiles_1 />
         <ProjectsSection isReflect={false} />
         <ProjectsSection isReflect={true} />
-        <CountingSection/>
+        <CountingSection />
       </main>
     </>
   );
--- src/components/sections/ExperienceSection.tsx
+++ src/components/sections/ExperienceSection.tsx
@@ -1,104 +1,47 @@
 "use client"
 import React from 'react';
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 
-const ExperienceSection = () => {
+interface Logo {
+  src: string;
+  alt: string;
+}
+
+interface ExperienceSectionProps {
+  title: string;
+  logos: Logo[];
+}
+
+const ExperienceSection: React.FC<ExperienceSectionProps> = ({ title, logos }) => {
   return (
     <>
       <div className="relative flex flex-col items-center overflow-hidden py-20 md:py-40">
         <div className="container relative z-[1] m-auto px-6 md:px-12">
           <div className="m-auto text-center md:w-8/12 lg:w-6/12">
             <h2 className="text-2xl font-bold text-gray-800 dark:text-white md:text-4xl">
-              My <span className="text-primary">{`Experience's`}</span> 
+              {title}
             </h2>
           </div>
           <div className="m-auto mt-16 md:w-11/12 lg:w-8/12 xl:w-7/12">
             <div className="flex flex-wrap justify-center gap-6">
-              <motion.div
-                className="group rounded-xl border border-gray-200 dark:border-gray-600 bg-white bg-opacity-5 py-2 px-4 hover:bg-opacity-10"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                <img
-                  src="/airbnb.svg"
-                  className="contrast-0 transition group-hover:contrast-100"
-                  loading="lazy"
-                  alt="logo airbnb"
-                />
-              </motion.div>
-              <motion.div
-                className="group rounded-xl border border-gray-200 dark:border-gray-600 bg-white bg-opacity-5 py-2 px-4 hover:bg-opacity-10"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                <img
-                  src="/microsoft.svg"
-                  className="contrast-0 transition group-hover:contrast-100"
-                  loading="lazy"
-                  alt="logo microsoft"
-                />
-              </motion.div>
-              <motion.div
-                className="group rounded-xl border border-gray-200 dark:border-gray-600 bg-white bg-opacity-5 py-2 px-4 hover:bg-opacity-10"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                <img
-                  src="/ge.svg"
-                  className="contrast-0 transition group-hover:contrast-100"
-                  loading="lazy"
-                  alt="logo ge"
-                />
-              </motion.div>
-              <motion.div
-                className="group rounded-xl border border-gray-200 dark:border-gray-600 bg-white bg-opacity-5 py-2 px-4 hover:bg-opacity-10"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                <img
-                  src="/bissell.svg"
-                  className="contrast-0 transition group-hover:contrast-100"
-                  loading="lazy"
-                  alt="logo bissell"
-                />
-              </motion.div>
-              <motion.div
-                className="group rounded-xl border border-gray-200 dark:border-gray-600 bg-white bg-opacity-5 py-2 px-4 hover:bg-opacity-10"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                <img
-                  src="/bissell.svg"
-                  className="contrast-0 transition group-hover:contrast-100"
-                  loading="lazy"
-                  alt="logo bissell"
-                />
-              </motion.div>
-              <motion.div
-                className="group rounded-xl border border-gray-200 dark:border-gray-600 bg-white bg-opacity-5 py-2 px-4 hover:bg-opacity-10"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                <img
-                  src="/automatic.svg"
-                  className="contrast-0 transition group-hover:contrast-100"
-                  loading="lazy"
-                  alt="logo automatic"
-                />
-              </motion.div>
-              {/* Added Next.js image */}
-              <motion.div
-                className="group rounded-xl border border-gray-200 dark:border-gray-600 bg-white bg-opacity-5 py-2 px-4 hover:bg-opacity-10"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                <img
-                  src="/nextjs.svg"
-                  className="contrast-0 transition group-hover:contrast-100"
-                  loading="lazy"
-                  alt="logo next.js"
-                />
-              </motion.div>
+              {logos.map((logo, index) => (
+                <motion.div
+                  key={index}
+                  className="group rounded-xl border border-gray-200 dark:border-gray-600 bg-white bg-opacity-5 py-2 px-4 hover:bg-opacity-10"
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                >
+                  <img
+                    src={logo.src}
+                    className="contrast-0 transition group-hover:contrast-100 max-h-9"
+                    loading="lazy"
+                    alt={logo.alt}
+                    title={logo.alt}
+                    
+                  />
+                </motion.div>
+              ))}
             </div>
           </div>
         </div>
@@ -107,5 +50,4 @@ const ExperienceSection = () => {
   );
 };
 
-export default 
-ExperienceSection;
+export default ExperienceSection;
