8a4cda5b47c5e13fe09bd022de1d2bfa283cccd8
suborno180 <64451255+suborno180@users.noreply.github.com>
2024-04-23 14:26:45 +0600

    update

--- src/components/FetcherCard.tsx
+++ src/components/FetcherCard.tsx
@@ -14,13 +14,13 @@ const FetcherCard: React.FC<FetcherCardProps> = ({ title, description, icon }) =
   return (
     <div className={`w-full group h-[380px] grid grid-rows-2 rounded-3xl bg-gray-800 hover:bg-gradient-to-t hover:from-green-800 hover:to-green-400 p-10 hover:drop-shadow-2xl`}>
       <div className='w-full h-full grid place-content-center'>
-        <div className='w-28 h-28 border-green-400 border group-hover:bg-gradient-to-b mb-4 from-emerald-600 to-green-400 rounded-full grid place-content-center'>
+        <div className='w-28 h-28 border-green-400 border group-hover:bg-gradient-to-b mb-4 from-emerald-600 to-green-400 rounded-full grid place-content-center text-white'>
           {icon}
         </div>
       </div>
       <div className='w-full h-full items-center text-center flex flex-col gap-4'>
-        <h1 className='text-2xl font-bold'>{title}</h1>
-        <p className='text-[18px]'>{description}</p>
+        <h1 className='text-2xl font-bold text-white'>{title}</h1>
+        <p className='text-[18px] text-white'>{description}</p>
       </div>
     </div>
   );
