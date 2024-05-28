78547eb40b3168dc5805e04da8e124153c1afe14
suborno180 <suborno.dev@gmail.com>
2024-05-28 12:20:55 +0600

    update

--- src/components/sections/ExperienceSection.tsx
+++ src/components/sections/ExperienceSection.tsx
@@ -14,6 +14,27 @@ interface ExperienceSectionProps {
   logos: Logo[];
 }
 
+
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2
+    }
+  }
+}
+  
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+}
+
 const ExperienceSection: React.FC<ExperienceSectionProps> = ({ title, logos }) => {
   return (
     <>
